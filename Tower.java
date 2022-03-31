146a0a9656cf2dbc249c674ad65af695af859491
kchesl <102710433+kchesl@users.noreply.github.com>
2022-03-30 23:10:26 -0400

    Update Tower.java

--- Tower.java
+++ Tower.java
@@ -1,16 +1,4 @@
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those who
-// do.
-// -- Kirsten Chesley (kchesley888)
-package towerofhanoi;
 
-/**
- *
- * @author Kirsten Chesley (kchesley888)
- * @version (2020-Mar-25)
- */
 public class Tower extends LinkedStack<Disk> {
     private Position position;
 
