90f5fdcb78a821758858ab224ea4dff7d78f9391
kchesl <102710433+kchesl@users.noreply.github.com>
2022-03-30 23:09:24 -0400

    Update PuzzleWindow.java

--- PuzzleWindow.java
+++ PuzzleWindow.java
@@ -1,10 +1,4 @@
-package towerofhanoi;
-// Virginia Tech Honor Code Pledge:
-//
-// As a Hokie, I will conduct myself with honor and integrity at all times.
-// I will not lie, cheat, or steal, nor will I accept the actions of those who
-// do.
-// -- Kirsten Chesley (kchesley888)
+
 
 import java.awt.Color;
 import java.util.Observable;
